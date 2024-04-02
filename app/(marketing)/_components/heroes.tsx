2a83fc0150b680ad3c724a38a2c90973081ce4ef
Asman Bhusal <abhusal1@outlook.com>
2024-04-02 19:01:57 +0100

    Landing Page

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export const Heroes = () => {
+    return (
+        <div className="flex flex-col items-center justify-center max-w-5xl">
+            <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+                <Image 
+                    src="/document-lightmode.svg"
+                    fill
+                    className="object-contain"
+                    alt="Documents"
+                />
+            </div>
+            <div className="relative h-[400px] w-[400px] hidden md:block">
+                <Image 
+                    src="/reading-lightmode.svg"
+                    fill
+                    className="object-contain"
+                    alt="Reading"
+                />
+            </div>
+        </div>
+    )
+}
