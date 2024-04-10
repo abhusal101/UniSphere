e665f6b8b73468919c5f6d7d5486e4ac0481f7fd
Asman Bhusal <abhusal1@outlook.com>
2024-04-11 00:36:02 +0100

    Document list added with recursive function

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { 
-    ChevronsLeft, 
-    MenuIcon, 
-    PlusCircle, 
-    Search, 
+    ChevronsLeft,
+    MenuIcon,
+    PlusCircle,
+    Search,
     Settings
 } from "lucide-react";
 
@@ -13,15 +13,15 @@ import { ElementRef, useEffect, useRef, useState} from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "@/lib/utils";
 import { UserItem } from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const documents = useQuery(api.documents.get);
     const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
@@ -167,13 +167,7 @@ export const Navigation = () => {
                     />
                 </div>
                 <div className="mt-4">
-                    {documents?.map((document) => (
-                        <p key={document._id}>
-                            {document.title}
-                        </p>
-                    )
-                    
-                    )}
+                    <DocumentList />
                 </div>
                 <div 
                     onMouseDown={handleMouseDown}
