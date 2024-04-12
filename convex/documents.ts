f0f361c3f0b47beed926da28a4afecad98821580
Asman Bhusal <abhusal1@outlook.com>
2024-04-12 10:05:16 +0100

    Archive functionality added to pages

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,65 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
+// creating archieve api endpoint
+export const archive = mutation ({
+    args: { id: v.id("documents") }, // pass the argument id of the document we want to archive
+    handler: async (ctx, args) => {
+        // checking the identity
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        // fetch the document using the above id
+        const existingDocument = await ctx.db.get(args.id);
+
+        if (!existingDocument) {
+            throw new Error("Not Found");
+        }
+
+        // checking if the userid matches the currently logged in user
+        if (existingDocument.userId !== userId) {
+            throw new Error("Unauthorised");
+        }
+
+        const recursiveArchive = async (documentId: Id<"documents">) => {
+            //fetching all the children which have that id as their parent id 
+            const children = await ctx.db
+                .query("documents")
+                .withIndex("by_user_parent", (q) => (
+                    q
+                        .eq("userId", userId)
+                        .eq("parentDocument", documentId)
+                ))
+                .collect();
+
+            // archive all the children
+            for (const child of children) {
+                await ctx.db.patch(child._id, {
+                    isArchived: true,
+                });
+
+                // checking if the children have children or not
+                await recursiveArchive(child._id);
+            }
+        }
+
+        //change/arhive the document
+        const document = await ctx.db.patch(args.id, {
+            isArchived: true,
+        });
+
+        // after the document is archived, passing the document id inside the recursive function
+        recursiveArchive(args.id);
+
+        return document;
+    }
+})
+
 export const getSidebar = query ({
     args: {
         parentDocument: v. optional(v.id("documents"))
